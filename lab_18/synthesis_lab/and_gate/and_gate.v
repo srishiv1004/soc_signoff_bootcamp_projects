85408cae4e11ebf2c2de657fc0b7d82cf257e497
Shivani S <srishivani104@gmail.com>
2026-09-06 08:26:48 +0000

    Lab 18 : mini_soc design using yosys

---
+++ lab_18/synthesis_lab/and_gate/and_gate.v
@@ -0,0 +1,4 @@
+module and_gate(input a,b, output y);
+assign y = a & b;
+endmodule
+
---
+++ lab_18/synthesis_lab/and_gate/and_gate_non_synth.v
@@ -0,0 +1,28 @@
+module and_gate_non_synth(
+	input a,b,
+	output reg y
+);
+
+initial begin
+	y = 0;
+end
+
+always @(a or b) begin
+	#5 y = a & b;
+end
+
+always @(y) begin
+	$display("TIME = %0t : a = %b b = %b y = %b", $time,a,b,y);
+end
+
+initial begin
+	$monitor("MONITOR :time= %0t a = 5b b = %b y = %b" ,$time,a,b,y);
+end
+
+initial begin
+	#120;
+	$display("simulation finished");
+	$finish;
+end
+endmodule
+
